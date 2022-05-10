6d1510bd9c72a8762d83e34ac3923f6c6ac6df27
yuri <yuri01051@outlook.com>
2022-05-10 23:00:21 +1000

    5_10_!

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,14 @@ import Chakra from '../components/chakra'
 
 import { useState, useEffect } from 'react';
 import Web3 from 'web3'
-
+import "../styles/main.css";
+const ROPSTEN_CHAIN_ID = 3;
 const web3 = new Web3(Web3.givenProvider);
-const contractAddress = '0xa9eB673BA77A9567d9062e810922a0eAc0192FFC';
-const contract_abi = [{ "inputs": [{ "internalType": "address", "name": "_admin", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "uint256", "name": "id", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "bet", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "randomSeed", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }, { "indexed": false, "internalType": "address", "name": "player", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "winAmount", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "randomResult", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "time", "type": "uint256" }], "name": "Result", "type": "event" }, { "inputs": [], "name": "MAX_DEPOSIT_AMOUNT", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "MIN_DEPOSIT_AMOUNT", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_addr", "type": "address" }], "name": "addManager", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "adminWallet", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_addr", "type": "address" }], "name": "changeAdminWallet", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "fund", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "bet", "type": "uint256" }, { "internalType": "uint256", "name": "seed", "type": "uint256" }], "name": "game", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "payable", "type": "function" }, { "inputs": [], "name": "gameId", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "games", "outputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }, { "internalType": "uint256", "name": "bet", "type": "uint256" }, { "internalType": "uint256", "name": "seed", "type": "uint256" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "address payable", "name": "player", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "lastGameId", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "managers", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_addr", "type": "address" }], "name": "removeManager", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "withdrawEther", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
+// const web3Provider = new Web3.providers.HttpProvider("https://ropsten.infura.io/v3/");
+// const web3 = new Web3(web3Provider);
+
+const contractAddress = '0x46553A069EE5A925C37A21Da67e3392C48E98708';
+const contract_abi = [{ "inputs": [{ "internalType": "address", "name": "_admin", "type": "address" }], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "uint256", "name": "id", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "bet", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "randomSeed", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }, { "indexed": false, "internalType": "address", "name": "player", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "winAmount", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "randomResult", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "time", "type": "uint256" }], "name": "Result", "type": "event" }, { "inputs": [], "name": "MAX_DEPOSIT_AMOUNT", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "MIN_DEPOSIT_AMOUNT", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_addr", "type": "address" }], "name": "addManager", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "adminWallet", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_addr", "type": "address" }], "name": "changeAdminWallet", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "fund", "outputs": [], "stateMutability": "payable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "bet", "type": "uint256" }, { "internalType": "uint256", "name": "seed", "type": "uint256" }], "name": "game", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "payable", "type": "function" }, { "inputs": [], "name": "gameId", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "name": "games", "outputs": [{ "internalType": "uint256", "name": "id", "type": "uint256" }, { "internalType": "uint256", "name": "bet", "type": "uint256" }, { "internalType": "uint256", "name": "seed", "type": "uint256" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "uint256", "name": "winAmount", "type": "uint256" }, { "internalType": "uint256", "name": "time", "type": "uint256" }, { "internalType": "address payable", "name": "player", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_addr", "type": "address" }], "name": "initManager", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }], "name": "managers", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_addr", "type": "address" }], "name": "removeManager", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "withdrawEther", "outputs": [], "stateMutability": "nonpayable", "type": "function" }];
 const contract = new web3.eth.Contract(contract_abi, contractAddress);
 
 if (typeof window !== 'undefined') {
@@ -42,8 +46,8 @@ function Website({ Component, pageProps, router }) {
         }
       });
 
-      window.ethereum.on("chainChanged", (_chainId) => {
-        if (_chainId != 3) {
+      window.ethereum.on("chainChanged", async (_chainId) => {
+        if (_chainId != ROPSTEN_CHAIN_ID) {
           setWallet("");
           setBalance(0);
         }
@@ -60,10 +64,11 @@ function Website({ Component, pageProps, router }) {
         method: "eth_chainId"
       });
 
-      if (chainId != 3) {
+      if (chainId != ROPSTEN_CHAIN_ID) {
         alert("Please switch to Ethereum network!");
         setWallet("");
         setBalance(0);
+        return;
       }
 
       try {
@@ -91,9 +96,10 @@ function Website({ Component, pageProps, router }) {
           method: "eth_chainId"
         });
 
-        if (chainId != 3) {
+        if (chainId != ROPSTEN_CHAIN_ID) {
           setWallet("");
           setBalance(0);
+          return;
         }
 
         const addressArray = await window.ethereum.request({
@@ -141,6 +147,9 @@ function Website({ Component, pageProps, router }) {
             contractAddress={contractAddress}
             balance={balance}
             setBalance={setBalance}
+            games={games}
+            setGames={setGames}
+            contract={contract}
           />
         </AnimatePresence>
       </Layout>
--- pages/betting.js
+++ pages/betting.js
@@ -26,12 +26,13 @@ import Router from 'next/router';
 const Betting = ({ wallet, web3, contract, contractAddress, balance, setBalance }) => {
     const [hound, setHound] = useState('');
     const [betAmount, setBetAmount] = useState();
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         if (wallet.length == 0) {
             return Router.push('/');
         }
-    }, []);
+    }, [wallet]);
 
     const onGreyClicked = () => {
         setHound('grey');
@@ -59,13 +60,14 @@ const Betting = ({ wallet, web3, contract, contractAddress, balance, setBalance 
 
         //Send bet to the contract and wait for the verdict
         contract.methods.game(bet, randomSeed).send({ from: wallet, value: amount }).on('transactionHash', (hash) => {
-            // this.setState({ loading: true })
-            contract.events.Result({}, async (error, event) => {
+            setLoading(true);
+            // contract.events.Result({}, async (error, event) => {
+            contract.once('Result', {}, async (error, event) => {
                 const verdict = event.returnValues.winAmount
                 if (verdict === '0') {
-                    alert('lose :(')
+                    alert('SORRY, UNFORTUNATELY YOU LOST :(')
                 } else {
-                    alert('WIN!')
+                    alert('CONGRATULATIONS! YOU WIN! :)')
                 }
 
                 //Prevent error when user logout, while waiting for the verdict
@@ -74,129 +76,161 @@ const Betting = ({ wallet, web3, contract, contractAddress, balance, setBalance 
                     balance = (Math.round(web3.utils.fromWei(balance) * 1000) / 1000).toFixed(3);
                     setBalance(balance);
                 }
-                //   this.setState({ loading: false })
+                setLoading(false);
             })
         }).on('error', (error) => {
-            alert('Error')
+            console.log('Error')
+            setLoading(false);
         })
     }
 
     return (
         <Layout>
             <Container>
-                <div
-                    style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}
-                >
-                    <img
-                        src="/images/houndsmoking1.png"
-                        width={300}
-                        height={300}
-                    />
-                </div>
+                {
+                    loading ? (
+                        <>
+                            <div
+                                style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}
+                            >
+                                <img
+                                    src="/images/grey_running.gif"
+                                    width={250}
+                                    height={250}
+                                />
+                            </div>
 
-                <Box
-                    borderRadius="lg"
-                    mb={6}
-                    p={3}
-                    textAlign="center"
-                    bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-                    css={{ backdropFilter: 'blur(10px)' }}
-                >
-                    Wallet Balance : {balance} ETH
-                </Box>
+                            <hr />
+                            <div
+                                style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}
+                            >
+                                <img
+                                    src="/images/orange_running.gif"
+                                    width={250}
+                                    height={250}
+                                />
+                            </div>
+                        </>
+                    ) :
+                        (
+                            <>
+                                <div
+                                    style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}
+                                >
+                                    <img
+                                        src="/images/houndsmoking1.png"
+                                        width={300}
+                                        height={300}
+                                    />
+                                </div>
 
-                <Box display={{ md: 'flex' }}>
-                    <Box
-                        flexGrow={1}
-                        textAlign="center"
-                    >
-                        <p>I LIKE </p>
-                    </Box>
-                </Box>
+                                <Box
+                                    borderRadius="lg"
+                                    mb={6}
+                                    p={3}
+                                    textAlign="center"
+                                    bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                                    css={{ backdropFilter: 'blur(10px)' }}
+                                >
+                                    Wallet Balance : {balance} ETH
+                                </Box>
 
-                <Box align="center" my={4}>
-                    <Button
-                        colorScheme={hound === 'grey' ? 'pink' : 'teal'}
-                        onClick={onGreyClicked}
-                    >
-                        GREY HOUND
-                    </Button>
-                    <Button
-                        colorScheme={hound === 'orange' ? 'pink' : 'teal'}
-                        style={{ marginLeft: 8 }}
-                        onClick={onOrangeClicked}
-                    >
-                        ORANGE HOUND
-                    </Button>
-                </Box>
+                                <Box display={{ md: 'flex' }}>
+                                    <Box
+                                        flexGrow={1}
+                                        textAlign="center"
+                                    >
+                                        <p>I LIKE </p>
+                                    </Box>
+                                </Box>
 
-                <Box display={{ md: 'flex' }}>
-                    <Box
-                        flexGrow={1}
-                        textAlign="center"
-                    >
-                        <p>FOR</p>
-                    </Box>
-                </Box>
+                                <Box align="center" my={4}>
+                                    <Button
+                                        colorScheme={hound === 'grey' ? 'pink' : 'teal'}
+                                        onClick={onGreyClicked}
+                                    >
+                                        GREY HOUND
+                                    </Button>
+                                    <Button
+                                        colorScheme={hound === 'orange' ? 'pink' : 'teal'}
+                                        style={{ marginLeft: 8 }}
+                                        onClick={onOrangeClicked}
+                                    >
+                                        ORANGE HOUND
+                                    </Button>
+                                </Box>
 
-                <Box align="center" my={4}>
-                    <Button
-                        colorScheme={betAmount == 0.01 ? 'pink' : 'teal'}
-                        onClick={() => onBetAmountClicked(0.01)}
-                    >
-                        0.01 ETH
-                    </Button>
-                    <Button
-                        colorScheme={betAmount == 0.025 ? 'pink' : 'teal'}
-                        style={{ marginLeft: 4 }}
-                        onClick={() => onBetAmountClicked(0.025)}
-                    >
-                        0.025 ETH
-                    </Button>
-                    <Button
-                        colorScheme={betAmount == 0.04 ? 'pink' : 'teal'}
-                        style={{ marginLeft: 4 }}
-                        onClick={() => onBetAmountClicked(0.04)}
-                    >
-                        0.04 ETH
-                    </Button>
-                </Box>
+                                <Box display={{ md: 'flex' }}>
+                                    <Box
+                                        flexGrow={1}
+                                        textAlign="center"
+                                    >
+                                        <p>FOR</p>
+                                    </Box>
+                                </Box>
 
-                <Box align="center" my={4}>
-                    <Button
-                        colorScheme={betAmount == 0.05 ? 'pink' : 'teal'}
-                        onClick={() => onBetAmountClicked(0.05)}
-                    >
-                        0.05 ETH
-                    </Button>
-                    <Button
-                        colorScheme={betAmount == 0.065 ? 'pink' : 'teal'}
-                        style={{ marginLeft: 4 }}
-                        onClick={() => onBetAmountClicked(0.065)}
-                    >
-                        0.065 ETH
-                    </Button>
-                    <Button
-                        colorScheme={betAmount == 0.08 ? 'pink' : 'teal'}
-                        style={{ marginLeft: 4 }}
-                        onClick={() => onBetAmountClicked(0.08)}
-                    >
-                        0.08 ETH
-                    </Button>
-                </Box>
+                                <Box align="center" my={4}>
+                                    <Button
+                                        colorScheme={betAmount == 0.01 ? 'pink' : 'teal'}
+                                        onClick={() => onBetAmountClicked(0.01)}
+                                    >
+                                        0.01 ETH
+                                    </Button>
+                                    <Button
+                                        colorScheme={betAmount == 0.025 ? 'pink' : 'teal'}
+                                        style={{ marginLeft: 4 }}
+                                        onClick={() => onBetAmountClicked(0.025)}
+                                    >
+                                        0.025 ETH
+                                    </Button>
+                                    <Button
+                                        colorScheme={betAmount == 0.04 ? 'pink' : 'teal'}
+                                        style={{ marginLeft: 4 }}
+                                        onClick={() => onBetAmountClicked(0.04)}
+                                    >
+                                        0.04 ETH
+                                    </Button>
+                                </Box>
 
-                <Box align="center" my={4}>
-                    <hr />
-                </Box>
+                                <Box align="center" my={4}>
+                                    <Button
+                                        colorScheme={betAmount == 0.05 ? 'pink' : 'teal'}
+                                        onClick={() => onBetAmountClicked(0.05)}
+                                    >
+                                        0.05 ETH
+                                    </Button>
+                                    <Button
+                                        colorScheme={betAmount == 0.065 ? 'pink' : 'teal'}
+                                        style={{ marginLeft: 4 }}
+                                        onClick={() => onBetAmountClicked(0.065)}
+                                    >
+                                        0.065 ETH
+                                    </Button>
+                                    <Button
+                                        colorScheme={betAmount == 0.08 ? 'pink' : 'teal'}
+                                        style={{ marginLeft: 4 }}
+                                        onClick={() => onBetAmountClicked(0.08)}
+                                    >
+                                        0.08 ETH
+                                    </Button>
+                                </Box>
 
-                <Box align="center" my={4}>
-                    <Button
-                        colorScheme="teal"
-                        onClick={onBetClicked}
-                    >
-                        DOUBLE OR NOTHING
-                    </Button>
-                </Box>
+                                <Box align="center" my={4}>
+                                    <hr />
+                                </Box>
+
+                                <Box align="center" my={4}>
+                                    <Button
+                                        colorScheme="teal"
+                                        onClick={onBetClicked}
+                                    >
+                                        DOUBLE OR NOTHING
+                                    </Button>
+                                </Box>
+                            </>
+                        )
+                }
+
             </Container>
         </Layout>
     )
